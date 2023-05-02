97dc2fc433a1f47f53cb7834ea6d913f938e74f4
olkm-ren-sasaki <sasaki.ren.infratop@gmail.com>
2023-05-02 21:19:20 +0900

    Initial

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,7 @@ class User < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    self.profile_image.variant(resize_to_fill: [weight,height]).processed
+      profile_image.variant(resize_to_fill: [weight,height]).processed
   end
 end
+
